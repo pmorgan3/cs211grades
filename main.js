c543678c39c07e55925d3447970da1441b834332
Paul Morgan III <pmorgan3@gmail.com>
2018-04-23 14:16:39 -0500

    Fixed algorithm

--- main.js
+++ main.js
@@ -88,14 +88,12 @@ function calculateAll() {
 }
 
 function calcFinalGrade(labs, reviews, projects, exam) {
-    var grade = 100 - (((.075 * (reviews / (countReview * 40) * 100)
-    + .375 * (labs / (countLabs * 100) * 100)
-    + .35 * (projects / (countProjects * 100) * 100)
-    + .08 * ((exam/100)*100))) /
-     (countLabs * 100 + countProjects * 100 
-        + countReview * 40 + 100)) * 100;
+    var grade = (((7.5 * (reviews / (countReview * 40))
+    + 37.5 * (labs / (countLabs * 100))
+    + 35 * (projects / (countProjects * 100))
+    + 8 * ((exam/100))))) / 88 * 100;
     var gradeText = document.createElement("h3");
-    var t = document.createTextNode(`${grade.toFixed(2)}`);
+    var t = document.createTextNode(`${grade.toFixed(2)}%`);
     gradeText.appendChild(t);
     document.getElementById("Grade").appendChild(gradeText);
     
